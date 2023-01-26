5dd844a3213c558fc95e12e41f8ceabdf38e677d
ahmetbugraozcan <bugra.ahmetozcan@gmail.com>
2023-01-26 23:23:25 +0300

    Son eklenenler ekranı grid'den listview'a taşındı. detay sayfasındaki ui hataları düzeltildi.

--- lib/View/WatchAnime/watch_anime_view.dart
+++ lib/View/WatchAnime/watch_anime_view.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterglobal/Core/Extensions/context_extensions.dart';
 
 import 'package:flutterglobal/Provider/anime/watch_anime_cubit.dart';
 import 'package:flutterglobal/Provider/anime/watch_anime_state.dart';
@@ -14,84 +15,93 @@ class WatchAnimeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text("Anime izle"),
       ),
-      body: BlocBuilder<WatchAnimeCubit, WatchAnimeState>(
-        buildWhen: (previous, current) =>
-            previous.isLoading != current.isLoading ||
-            previous.animeEpisodeList != current.animeEpisodeList,
-        bloc: context.read<WatchAnimeCubit>(),
-        builder: (context, state) {
-          if (state.isLoading) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (state.animeEpisodeList.isEmpty) {
-            return Center(child: Text("Anime listesi boş"));
-          }
-          return SafeArea(
-            child: GridView.builder(
-                itemCount: state.animeEpisodeList.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.7,
-                ),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: InkWell(
-                      onTap: () {
-                        // context
-                        //     .read<WatchAnimeCubit>()
-                        //     .setSelectedAnimeEpisode(
-                        //         state.animeEpisodeList[index]);
-
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => WatchAnimeDetailsView(
-                              cubit: WatchAnimeDetailsCubit(
-                                  animeEpisode: state.animeEpisodeList[index]),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 12.0, left: 12.0),
+              child: Text("Son Eklenen Bölümler",
+                  style: context.textTheme.headlineSmall),
+            ),
+            Container(
+              padding: EdgeInsets.all(8.0),
+              height: context.height / 3,
+              color: Colors.red,
+              child: BlocBuilder<WatchAnimeCubit, WatchAnimeState>(
+                buildWhen: (previous, current) =>
+                    previous.isLoading != current.isLoading ||
+                    previous.animeEpisodeList != current.animeEpisodeList,
+                bloc: context.read<WatchAnimeCubit>(),
+                builder: (context, state) {
+                  if (state.isLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                  if (state.animeEpisodeList.isEmpty) {
+                    return Center(child: Text("Anime listesi boş"));
+                  }
+                  return ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: state.animeEpisodeList.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => WatchAnimeDetailsView(
+                                    cubit: WatchAnimeDetailsCubit(
+                                        animeEpisode:
+                                            state.animeEpisodeList[index]),
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Card(
+                              clipBehavior: Clip.antiAlias,
+                              color: Colors.white,
+                              child: Column(
+                                children: [
+                                  Expanded(
+                                    child: CachedNetworkImage(
+                                        placeholder: (context, url) => Center(
+                                              child:
+                                                  CircularProgressIndicator(),
+                                            ),
+                                        imageUrl: state.animeEpisodeList[index]
+                                                .thumbnail ??
+                                            state.animeEpisodeList[index]
+                                                .animeImage ??
+                                            ""),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0, vertical: 3.0),
+                                    child: Text(state.animeEpisodeList[index]
+                                            .episodeDescription ??
+                                        ""),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         );
-                      },
-                      child: Card(
-                        clipBehavior: Clip.antiAlias,
-                        color: Colors.white,
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: CachedNetworkImage(
-                                  placeholder: (context, url) => Center(
-                                        child: CircularProgressIndicator(),
-                                      ),
-                                  imageUrl:
-                                      state.animeEpisodeList[index].thumbnail ??
-                                          state.animeEpisodeList[index]
-                                              .animeImage ??
-                                          ""),
-                              // child: Image.network(
-                              //   state.animeEpisodeList[index].thumbnail ?? "",
-                              //   fit: BoxFit.fill,
-                              // ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 8.0, vertical: 3.0),
-                              child: Text(state.animeEpisodeList[index]
-                                      .episodeDescription ??
-                                  ""),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-          );
-        },
+                      });
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 12.0, left: 12.0),
+              child: Text("Son Eklenen Animeler",
+                  style: context.textTheme.headlineSmall),
+            )
+          ],
+        ),
       ),
     );
   }
